87e03200ef3bebb9ef3ec346abfda5569da9c751
Passawat Wetchasart <markpassawat.dev@gmail.com>
2026-08-29 00:28:09 +0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,15 @@
+func twoSum(nums []int, target int) []int {
+    indices := make(map[int]int)
+
+    for i, n := range nums {
+        indices[n] = i
+    }
+
+    for i, n := range nums {
+        diff := target - n
+        if j, found := indices[diff]; found && j != i {
+            return []int{i, j}
+        }
+    }
+    return []int{}
+}
